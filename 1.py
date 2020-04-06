ba83d57d71f728eecafb5bb6c2abc239643c9ef3
li-1999 <63014219+li-1999@users.noreply.github.com>
2020-04-06 10:34:50 +0800

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+
+
+dates=pd.date_range('20130101',periods=6)
+df=pd.DataFrame(np.arange(24).reshape((6,4)),index=dates,columns=['A','B','C','D'])
+
+#print(df['A'],df.A)
+
+#print(df.loc['20130101',['A','B']])
+#print(df.iloc[3,1])
+#print(df.ix[:3,['A','C']])
+#print(df[df.A>8])
+
+df.iloc[2,2]=111
+df.loc["20130101",'B']=222
+df.B[df.A>4]=0
+df['F']=np.nan
+df['E']=pd.Series([1,2,3,4,5,6],index=pd.date_range('20130101',periods=6))
+print(df.dropna(axis=0,how='any'))
+
+print(df.fillna(value=0))
+
+
+
